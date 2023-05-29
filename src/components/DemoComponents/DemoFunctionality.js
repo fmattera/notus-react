55b614417a0618221ce79e477e4f520c45b5f4a4
FelixDeMan <deman.janfelix@gmail.com>
2023-05-29 18:37:13 +0200

    fguck yy

--- src/components/DemoComponents/DemoFunctionality.js
+++ src/components/DemoComponents/DemoFunctionality.js
@@ -4,7 +4,29 @@ import 'react-image-crop/dist/ReactCrop.css';
 import IndexNavbar from 'components/Navbars/IndexNavbar.js';
 import { FileUploader } from "baseui/file-uploader";
 import { FaQuestionCircle } from 'react-icons/fa';
-import image1 from 'assets/img/landskrona-dark_leather-3seat_denoisedv2.jpg';
+import asarum from 'assets/img/item_pictures/asarum.png';
+import ekanasetBeige from 'assets/img/item_pictures/ekanaset-beige.png';
+import ekenasetGreen from 'assets/img/item_pictures/ekenaset-green.png';
+import essebodaBlue from 'assets/img/item_pictures/esseboda-blue.png';
+import fammarpBeige from 'assets/img/item_pictures/fammarp-beige.png';
+import glostad2SeatSofa from 'assets/img/item_pictures/glostad-2-seat-sofa-knisa-dark-grey__0950864_pe800736_s5 Background Removed.png';
+import kivik3SeatSofaKelingeGreyTurquoise from 'assets/img/item_pictures/kivik-3-seat-sofa-kelinge-grey-turquoise__1055806_pe848110_s5 Background Removed.png';
+import kivik3SeatSofaTallmyraWhiteBlack from 'assets/img/item_pictures/kivik-3-seat-sofa-tallmyra-white-black__0781544_pe760855_s5 Background Removed.png';
+import kivik3SeatSofaWithChaiseLongueKelingeGreyTurquoise from 'assets/img/item_pictures/kivik-3-seat-sofa-with-chaise-longue-kelinge-grey-turquoise__1055847_pe848125_s5 Background Removed.png';
+import klippan2SeatSofaVissleGrey from 'assets/img/item_pictures/klippan-2-seat-sofa-vissle-grey__0239990_pe379591_s5 Background Removed.png';
+import landskrona3SeatSofaGunnaredDarkGreyMetal from 'assets/img/item_pictures/landskrona-3-seat-sofa-gunnared-dark-grey-metal__0602115_pe680184_s5 Background Removed.png';
+import landskronaThreeSeatSofaGrannBomstadBlackMetal from 'assets/img/item_pictures/landskrona-three-seat-sofa-grann-bomstad-black-metal__0321002_pe514786_s5 Background Removed.png';
+import langaryd3SeatSofaLejdeLightGreyWood from 'assets/img/item_pictures/langaryd-3-seat-sofa-lejde-light-grey-wood__0992758_pe820227_s5 Background Removed.png';
+import linanas2SeatSofaVissleDarkGrey from 'assets/img/item_pictures/linanas-2-seat-sofa-vissle-dark-grey__0962404_pe808061_s5 Background Removed.png';
+import lyckseleHavet2SeatSofaBedVansbroDarkGrey from 'assets/img/item_pictures/lycksele-havet-2-seat-sofa-bed-vansbro-dark-grey__0949727_pe799964_s5 Background Removed.png';
+import napper from 'assets/img/item_pictures/napper.jpg';
+import parup3SeatSofaVissleDarkGreen from 'assets/img/item_pictures/parup-3-seat-sofa-vissle-dark-green__1041906_pe841186_s5 Background Removed.png';
+import smedstorp2SeatSofaLejdeRedBrownBlack from 'assets/img/item_pictures/smedstorp-2-seat-sofa-lejde-red-brown-black__0989821_pe818623_s5 Background Removed.png';
+import soderhamn4SeatSofaWithChaiseLongueGunnaredBeige from 'assets/img/item_pictures/soderhamn-4-seat-sofa-with-chaise-longue-gunnared-beige__0766708_pe753869_s5 Background Removed.png';
+import vimle2SeatSofaGrannBomstadBlack from 'assets/img/item_pictures/vimle-2-seat-sofa-grann-bomstad-black__0817321_pe773961_s5 Background Removed.png';
+import vimle2SeatSofaHallarpGrey from 'assets/img/item_pictures/vimle-2-seat-sofa-hallarp-grey__0949417_pe799719_s5 Background Removed.png';
+import viskafors3SeatSofaHogalidBrownBirch from 'assets/img/item_pictures/viskafors-3-seat-sofa-hogalid-brown-birch__1088056_pe861031_s5 Background Removed.png';
+
 //import image2 from './favicon.ico';
 
 
@@ -21,15 +43,17 @@ const DemoFunctionality = () => {
   const [showPopover, setShowPopover] = useState(false);
 
   const items = [
-    { src: image1, value: 'smedstorp-red-3seat_denoisedv2' },
-    { src: image1, value: 'buy' },
-    { src: image1, value: 'tu'  },
-    { src: image1, value: 'tu2' },
-    { src: image1, value: 'tu3' },
-    { src: image1, value: 'tu4' },
-    { src: image1, value: 'tu5' },
-    // Add more items here...
-  ];
+    { src: ekenasetGreen, value: 'ekanaset-green-3seat-green_denoisedv2' },
+    { src: essebodaBlue, value: 'esseboda-blue-2seat-blue_denoisedv2' },
+    { src: fammarpBeige, value: 'fammarp-beige-2seat-grey_denoisedv2' },
+    { src: glostad2SeatSofa, value: 'glostad-grey-2seat-grey_denoisedv2' },
+    { src: kivik3SeatSofaTallmyraWhiteBlack, value: 'kivik-beige-3seat-beige_denoisedv2' },
+    { src: landskronaThreeSeatSofaGrannBomstadBlackMetal, value: 'landskrona-dark_leather-3seat_denoisedv2' },
+    { src: langaryd3SeatSofaLejdeLightGreyWood, value: 'langaryd-grey-3seat_denoisedv2' },
+    { src: smedstorp2SeatSofaLejdeRedBrownBlack, value: 'smedstorp-red-3seat_denoisedv2' },
+    { src: vimle2SeatSofaGrannBomstadBlack, value: 'smedstorp-red-3seat_denoisedv2' },
+   ];
+  
 
 
   const handleSave = () => {
@@ -174,26 +198,28 @@ const DemoFunctionality = () => {
            
           </div>
         </div>
-          <div className="container mx-auto my-1">
-        <div className="flex justify-center">
+      {src && (
+        <div className="container mx-auto my-1">
+          <div className="flex justify-center">
 
-          <div className="p-8 bg-gray-200 rounded-lg shadow-lg lg:w-5/12 border-4 border-gray-400">
-          <div className="absolute top-0 right-0 mt-2 mr-2">
-              <FaQuestionCircle
-                className="text-gray-500 cursor-pointer"
-                size={16}
-                onMouseEnter={() => setShowPopover(true)}
-                onMouseLeave={() => setShowPopover(false)}
-              />
-            </div>
+            <div className="p-8 bg-gray-200 rounded-lg shadow-lg lg:w-5/12 border-4 border-gray-400">
+              <div className="flex justify-end">
+                <FaQuestionCircle
+                  className="text-gray-500 cursor-pointer mb-2"
+                  size={20}
+                  onMouseEnter={() => setShowPopover(true)}
+                  onMouseLeave={() => setShowPopover(false)}
+                />
+              </div>
+          
             {showPopover && (
-              <div className="absolute top-0 right-0 mt-10 mr-10 bg-white border border-gray-400 rounded-lg p-4">
+              <div className="mr-10 bg-white border border-gray-400 rounded-lg p-4">
                 <p>Please make the image square.</p>
               </div>
             )}
             <div>
               
-              {src && (
+              
                 
                 <ReactCrop
                   key={reactCropKey}
@@ -213,32 +239,38 @@ const DemoFunctionality = () => {
                     style={{ maxHeight: '512px', maxWidth: '512px' }}
                   />
                 </ReactCrop>
-              )}
+              
             </div>
           </div>
         </div>
+      </div>
+      )}
         <div>
 
 
 
-        <div>
-          {completedCrop && (
-            <div className="flex justify-center ">
-              <div className="flex  overflow-x-auto rounded shadow-lg bg-white" style={{ width: 'fit-content' }}>
-                {items.map((item) => (
-                  <img
-                    key={item.value}
-                    src={item.src}
-                    alt={item.value}
-                    onClick={() => setSelectedItem(item.value)}
-                    className={`h-18 w-18 object-cover cursor-pointer `}
-                    style={{ width: selectedItem === item.value ? '70%' : '250px', height: selectedItem === item.value ? '70%' : '250px' }}
-                  />
-                ))}
+        {completedCrop && (
+          <div className="container mx-auto my-1">
+            <div className="mt-4">
+              <div className="max-w-3xl mx-auto overflow-y-auto rounded shadow-lg bg-white" style={{ maxHeight: '300px' }}>
+                <div className="flex justify-center" style={{ width: 'fit-content' }}>
+                  {items.map((item) => (
+                    <img
+                      key={item.value}
+                      src={item.src}
+                      alt={item.value}
+                      onClick={() => setSelectedItem(item.value)}
+                      className={`h-18 w-18 object-cover cursor-pointer`}
+                      style={{ width: selectedItem === item.value ? '120px' : '150px', height: selectedItem === item.value ? '120px' : '150px' }}
+                    />
+                  ))}
+                </div>
               </div>
             </div>
-          )}
-        </div>
+          </div>
+        )}
+
+
       </div>
       <div className="container mx-auto my-1">
         {completedCrop && (
@@ -259,7 +291,7 @@ const DemoFunctionality = () => {
           )}
         </div>
       </div>
-    </div>
+  
     </> );
 };  
 export default DemoFunctionality;
